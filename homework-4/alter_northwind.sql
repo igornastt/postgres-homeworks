bbe4020733fb7b4a16a921a8a0075284d2df3e81
Igor <100830659+igornastt@users.noreply.github.com>
2023-07-05 20:08:45 +0300

    Update alter_northwind.sql

--- homework-4/alter_northwind.sql
+++ homework-4/alter_northwind.sql
@@ -2,12 +2,12 @@
 -- 1. Добавить ограничение на поле unit_price таблицы products (цена должна быть больше 0)
 
 ALTER TABLE products    
-ADD	CONSTRAINT chk_products_unit_price CHECK (unit_price > 0)
+ADD CONSTRAINT chk_products_unit_price CHECK (unit_price > 0)
 
 -- 2. Добавить ограничение, что поле discontinued таблицы products может содержать только значения 0 или 1
 
 ALTER TABLE products    
-ADD	CONSTRAINT chk_products_discontinued CHECK (discontinued IN (0, 1))
+ADD CONSTRAINT chk_products_discontinued CHECK (discontinued IN (0, 1))
 
 -- 3. Создать новую таблицу, содержащую все продукты, снятые с продажи (discontinued = 1)
 
